a96683095c3a4e86faceefc71159c4f8e12cc641
AmazingAhmad <73358704+AmazingAhmad@users.noreply.github.com>
2024-10-03 23:32:29 +0530

    Add files via upload

---
+++ Make Sum Divisible by P.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) 
+    {
+        int n = nums.size(), tar = 0;
+        unordered_map<int, int> um;
+        for(int i=0;i<n;i++)
+            tar = (tar + nums[i]) % p;
+
+        if(tar == 0)
+            return 0;
+        
+        int preSum = 0, res = n;
+        um[0] = -1;
+        for(int i=0;i<n;i++)
+        {
+            preSum = (preSum + nums[i]) % p;
+            int need = (preSum - tar + p) % p;
+            if(um.find(need) != um.end())
+                res = min(res, i - um[need]);
+            um[preSum] = i;
+        }
+        if(res == n)
+            return -1;
+        return res;
+    }
+};
